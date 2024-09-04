b202f3dbbaa8cdba869b5e0df042b7c7d45b209a
Szymon22sito <szymon.sitarczukss@wp.pl>
2024-09-04 13:40:43 +0200

    losowanie liczby rzeczywistej

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("na lekcji");
+
+        double wylosowana = Math.random()*10+1;
+        System.out.println(wylosowana);
+        //git int
+        // git config user.email
+        //             user.name
+        //git add
+        //git commit -m ""
+        //git log
+        //git push -u orgin master
     }
 }
