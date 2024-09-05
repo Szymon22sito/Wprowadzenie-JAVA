e96c2adf91d5fe626b79a37b369250b51f87a874
Szymon22sito <szymon.sitarczukss@wp.pl>
2024-09-05 08:46:11 +0200

    zgadywanie 10 razy

--- src/Main.java
+++ src/Main.java
@@ -64,5 +64,23 @@ public class Main {
                     default -> "Słabo"
                 }
         );
+
+        //zgadywanie 10 razy
+        for (int i = 0; i < ; i++) {
+            System.out.println("Zgadnij liczbę, prba nr "+(i+1);
+            wpisanaLiczba = klawiatura.nextInt();
+            if (wpisanaLiczba == wylosowana){
+                System.out.println("Wygrana");
+                break;
+            }
+            else {
+                if (wpisanaLiczba>wylosowana){
+                    System.out.println("Wpisano za dużo");
+                }
+                else {
+                    System.out.println("Wpisano za mało");
+                }
+            }
+        }
     }
 }
